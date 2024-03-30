6642525779c2847ea95af44524e0b898a7f79c10
onurincesu <95099971+onurincesu@users.noreply.github.com>
2024-03-30 14:01:09 +0000

    Değişiklikler yapıldı.

--- streamlit-site.py
+++ streamlit-site.py
@@ -17,7 +17,7 @@ with st.sidebar:
     max_depth = st.slider("Max Depth", min_value=1, max_value=10, value=3, help="The maximum depth of each tree in the XGBoost or LightGBM model. Increasing the max depth can make the model more complex and potentially overfit the data.")
     learning_rate = st.slider("Learning Rate", min_value=0.01, max_value=1.0, value=0.01, help="The learning rate controls the step size at each boosting iteration. A lower learning rate makes the model learn more slowly but can lead to better generalization.")
     n_estimators = st.slider("Number of Estimators", min_value=1, max_value=1000, value=10, help="The number of boosting iterations or trees in the XGBoost or LightGBM model. Increasing the number of estimators can improve the model's performance, but also increase the training time.")
-    böl = st.slider("Split Size", min_value=0.1, max_value=1.0, value=0.3, help="The split size determines the proportion of data used for training and testing. A higher split size means more data is used for training, while a lower split size means more data is used for testing.")
+    böl = st.slider("Split Size", min_value=0.1, max_value=1.0, value=0.3, help="The split size determines the proportion of data used for training and testing. A higher split size means more data is used for training while a lower split size means more data is used for testing.")
     st.write("[Github](https://github.com/onurincesu)")
     st.write("[Linkedin](https://www.linkedin.com/in/ali-onur-incesu-04bb59218/)")
 def model_seç():
